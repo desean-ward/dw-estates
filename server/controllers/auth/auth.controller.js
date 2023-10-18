d2593efa73a9c4078b0b7710df23e91be5ff03bb
De Sean Ward <d.ward.chi@gmail.com>
2023-10-17 20:05:02 -0500

    Signin route w/JWT created

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../../models/user/user.modal");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Note: The 'next' parameter is used to call the next middleware function in the stack.
 const signup = async (req, res, next) => {
@@ -20,10 +21,43 @@ const signup = async (req, res, next) => {
       data: user,
     });
   } catch (error) {
+    console.log("ERROR", error.message);
+    next(error.message);
+  }
+};
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // Note: The 'password' field is removed from the user object before sending it to the client.
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "User logged in successfully",
+        data: rest,
+      });
+  } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
   signup,
+  signin,
 };
--- server/routes/auth/auth.route.js
+++ server/routes/auth/auth.route.js
@@ -4,5 +4,6 @@ const authCtrl = require("../../controllers/auth/auth.controller");
 const router = express.Router();
 
 router.post("/signup", authCtrl.signup);
+router.post("/signin", authCtrl.signin);
 
 module.exports = router;
