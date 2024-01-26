335bb02ed965da31668bf015e60c13f4f2aad88a
De Sean Ward <d.ward.chi@gmail.com>
2024-01-25 19:49:33 -0600

    Updated footer info

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -3,6 +3,13 @@ import {
   FooterContainer,
   FooterContent,
   FooterCopyright,
+  FooterTopSection,
+  InfoField,
+  InfoIcon,
+  InfoText,
+  Left,
+  Middle,
+  Right,
   SocialIcon,
   SocialIconsContainer,
 } from "./footer.styles";
@@ -10,35 +17,91 @@ import {
 import {
   FaFacebookF,
   FaInstagram,
+  FaPhoneAlt,
   FaPinterestP,
   FaTiktok,
   FaTwitter,
 } from "react-icons/fa";
 
+import { FaLocationDot } from "react-icons/fa6";
+
+import { MdEmail } from "react-icons/md";
+
 const Footer = () => {
   return (
     <FooterContainer>
       <FooterContent>
-        <SocialIconsContainer>
-          <SocialIcon>
-            <FaFacebookF color='#000' />
-          </SocialIcon>
-          <SocialIcon>
-            <FaTwitter color='#000' />
-          </SocialIcon>
-          <SocialIcon>
-            <FaTiktok color='#000' />
-          </SocialIcon>
-          <SocialIcon>
-            <FaPinterestP color='#000' />
-          </SocialIcon>
-          <SocialIcon>
-            <FaInstagram color='#000' />
-          </SocialIcon>
-        </SocialIconsContainer>
+        <FooterTopSection>
+          <Left>
+            <h2 className='flex flex-wrap text-2xl font-bold md:gap-2'>
+              <span>
+                <span className='text-[var(--clr-text-accent)]'>Luxe</span>
+                Living
+              </span>
+              Estates
+            </h2>
+            <p className='py-4 text-left'>
+              Your dependable real estate companion in enchanting Rarotonga,
+              presenting a collection of dream properties and promising
+              investment opportunities.
+            </p>
+          </Left>
 
+          <Middle>
+            <span className='font-bold'>USA</span>
+            <InfoField>
+              <InfoIcon>
+                <FaLocationDot />
+              </InfoIcon>
+              <InfoText>
+                123 Madison Street, Suite 123 <br />
+                Chicago, Illinois 60601
+              </InfoText>
+            </InfoField>
+
+            <InfoField>
+              <InfoIcon>
+                <MdEmail />
+              </InfoIcon>
+              <InfoText>dward@desean-ward.me</InfoText>
+            </InfoField>
+
+            <InfoField>
+              <InfoIcon>
+                <FaPhoneAlt />
+              </InfoIcon>
+              <InfoText>+1 773-209-1814</InfoText>
+            </InfoField>
+          </Middle>
+
+          <Right>
+            <p className='px-2 font-bold'>Follow Us</p>
+            <SocialIconsContainer>
+              <SocialIcon>
+                <FaFacebookF color='#000' />
+              </SocialIcon>
+              <SocialIcon>
+                <FaTwitter color='#000' />
+              </SocialIcon>
+              <SocialIcon>
+                <FaTiktok color='#000' />
+              </SocialIcon>
+              <SocialIcon>
+                <FaPinterestP color='#000' />
+              </SocialIcon>
+              <SocialIcon>
+                <FaInstagram color='#000' />
+              </SocialIcon>
+            </SocialIconsContainer>
+          </Right>
+        </FooterTopSection>
+
+        <section className='w-full'>
+          <hr />
+        </section>
         <FooterCopyright>
           &copy; {new Date().getFullYear()} LuxeLiving Estates
+          <span>De Sean Ward Dezignz</span>
         </FooterCopyright>
       </FooterContent>
     </FooterContainer>
--- client/src/components/footer/footer.styles.jsx
+++ client/src/components/footer/footer.styles.jsx
@@ -1,21 +1,68 @@
+import Link from "next/link";
 import tw from "tailwind-styled-components";
 
 export const FooterContainer = tw.footer`
     relative
     bottom-0
+    pt-16
 `;
 
 export const FooterContent = tw.div`
     flex 
     flex-col
     items-center
+    gap-8
     w-full 
     max-w-7xl
     mx-auto
 `;
 
-export const FooterCopyright = tw.section`
+export const FooterTopSection = tw.div`
+    flex 
+    flex-col md:flex-row
+    gap-8
+`;
 
+export const Left = tw.section`
+    w-full
+    px-4
+`;
+
+export const Middle = tw.section`
+    w-full
+    flex flex-col
+    gap-2
+    px-4
+    text-white
+`;
+
+export const Right = tw.section`
+    w-full
+    px-4
+`;
+
+export const InfoField = tw.section`
+    flex
+    items-center
+    gap-2
+    my-2
+`;
+
+export const InfoIcon = tw.section`
+    w-6 h-6
+`;
+
+export const InfoText = tw.section`
+    w-full
+`;
+
+export const FooterCopyright = tw.section`
+    w-full
+    flex 
+    flex-col-reverse md:flex-row
+    justify-between
+    text-center md:text-left
+    pb-8
 `;
 
 export const SocialIconsContainer = tw.section`
